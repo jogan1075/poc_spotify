b0922df3b2a750be84d9fa7eb7d3c61b9c974e50
Jonathan Muñoz <jmunoz@technisys.com>
2017-07-27 18:11:41 -0300

    init

---
+++ poc_spotify/app/src/main/java/jmc/com/poc_spotify/MainActivity.java
@@ -0,0 +1,16 @@
+package jmc.com.poc_spotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Client ID e573db3729ba460b9ebcb6ec02e7e849
+//        Client Secret 8655506ed3bc42f8aa13fa8acafccd93
+    }
+}
